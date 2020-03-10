a830b5377321058c8ed855f1f1ebe037a5ea89ff
cutiechi <superchijinpeng@gmail.com>
2020-03-10 23:41:52 +0800

    feat: add newAccountPage page

---
+++ src/views/NewAccountPage.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="new-account-page">
+    <p class="new-account-page-title">新建客户</p>
+    <div class="dividing-line"></div>
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <div class="">
+        <img class="base-information-icon" src="../assets/images/newAccountPage/arrow.png" alt="">
+        <el-collapse-item title="基础信息" name="1">
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-row>
+              <el-col :span="6">
+                <el-form-item label="活动名称" prop="name">
+                  <el-input v-model="ruleForm.name"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="6"></el-col>
+              <el-col :span="6"></el-col>
+              <el-col :span="6">
+                <el-form-item label="活动名称" prop="name">
+                  <el-input v-model="ruleForm.name"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-form-item label="活动区域" prop="region">
+              <el-select v-model="value5" multiple placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <el-select
+                v-model="value11"
+                multiple
+                collapse-tags
+                style="margin-left: 20px;"
+                placeholder="请选择">
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="活动形式" prop="pass">
+              <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+              <el-button @click="resetForm('ruleForm')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-collapse-item>
+      </div>
+    </el-collapse>
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '新建客户'
+  },
+  data () {
+    return {
+      activeNames: ['1'],
+      ruleForm: {
+        name: '',
+        region: '',
+        pass: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: '请选择活动区域', trigger: 'change' }
+        ],
+        desc: [
+          { required: true, message: '请填写活动形式', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleChange (val) {
+      console.log(val)
+    },
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style>
+.new-account-page{
+  width: 94%;
+  margin: 0 auto;
+  margin-top: 40px;
+}
+.new-account-page-title{
+  font-size: 20px;
+  color: #333;
+  text-align: left;
+  line-height: 10px;
+}
+.dividing-line{
+  width: 100%;
+  height: 1px;
+  background-color: #DCDFE6;
+  margin-top: 25px;
+}
+.base-information-icon{
+    margin-top: 13px;
+    width: 17px;
+    padding-right: 10px;
+    float: left;
+}
+</style>
