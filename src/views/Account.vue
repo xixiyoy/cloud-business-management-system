6891c8a10d08dc1c32984c8c985a2ef53b8cb265
cutiechi <superchijinpeng@gmail.com>
2020-03-23 21:34:20 +0800

    featur: add account page logic

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -4,21 +4,20 @@
         <el-row :gutter="20" class="header-custom">
           <el-col :span="3">
             <div class="grid-content bg-purple">
-              <el-button type="primary" @click="handleClickCreateAccount">新建</el-button>
+              <el-button type="primary" @click="handleClickCreateAccount">新  建</el-button>
             </div>
           </el-col>
-          <el-col :span="3"><div class="grid-content bg-purple">
+          <el-col :span="13"><div class="grid-content bg-purple">
             <el-button class="new-seetings">
               <img id="u829_img" class="img " src="../assets/images/account/u829.png">
               <span style="padding-left:10px;">导出</span>
             </el-button></div></el-col>
-          <el-col :span="11" style="margin-right:25px;"><div class="grid-content bg-purple"></div></el-col>
           <el-col :span="2">
             <div class="grid-content bg-purple">
-              <el-dropdown class="account-home-dropdown">
-                <span class="el-dropdown-link" style="color:#606266;">
+              <el-dropdown>
+                <el-button>
                   请选择<i class="el-icon-arrow-down el-icon--right"></i>
-                </span>
+                </el-button>
                 <el-dropdown-menu slot="dropdown">
                   <el-dropdown-item>服务中</el-dropdown-item>
                   <el-dropdown-item>已完成</el-dropdown-item>
@@ -115,67 +114,60 @@
         </el-row>
     </div>
     <div class="account-table-custom">
-      <el-tabs type="border-card">
-        <el-tab-pane>
-          <span slot="label">全部</span>
-          我的行程
+      <el-tabs type="border-card" v-model="activeTabName">
+        <el-tab-pane v-for="(tab, index) in accountTabs" :key="index" :label="tab.label" :name="tab.name">
+          <el-table
+            ref="multipleTable"
+            :data="accountTabsData"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange">
+            <el-table-column
+              type="selection">
+            </el-table-column>
+            <el-table-column
+              prop="accountName"
+              label="客户名称">
+            </el-table-column>
+            <el-table-column
+              prop="accountSource"
+              label="客户来源">
+            </el-table-column>
+            <el-table-column
+              prop="contactPerson"
+              label="联系人">
+              <template slot-scope="scope">{{ scope.row.date }}</template>
+            </el-table-column>
+            <el-table-column
+              prop="contactNumber"
+              label="联系电话">
+            </el-table-column>
+            <el-table-column
+              prop="accountStatus"
+              label="客户状态"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              prop="accountGrade"
+              label="客户等级"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              prop="SalesRepresentative"
+              label="销售代表"
+              show-overflow-tooltip>
+            </el-table-column>
+            <el-table-column
+              prop="OrderTotal"
+              label="订单总额">
+            </el-table-column>
+            <el-table-column
+              prop="operating"
+              label="操作">
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
-        <el-tab-pane label="我负责">消息中心</el-tab-pane>
-        <el-tab-pane label="我创建">角色管理</el-tab-pane>
-        <el-tab-pane label="部门">定时任务补偿</el-tab-pane>
       </el-tabs>
-      <el-table
-        ref="multipleTable"
-        :data="tableData3"
-        tooltip-effect="dark"
-        style="width: 100%"
-        @selection-change="handleSelectionChange">
-        <el-table-column
-          type="selection">
-        </el-table-column>
-        <el-table-column
-          prop="accountName"
-          label="客户名称">
-        </el-table-column>
-        <el-table-column
-          prop="accountSource"
-          label="客户来源">
-        </el-table-column>
-        <el-table-column
-          prop="contactPerson"
-          label="联系人">
-          <template slot-scope="scope">{{ scope.row.date }}</template>
-        </el-table-column>
-        <el-table-column
-          prop="contactNumber"
-          label="联系电话">
-        </el-table-column>
-        <el-table-column
-          prop="accountStatus"
-          label="客户状态"
-          show-overflow-tooltip>
-        </el-table-column>
-        <el-table-column
-          prop="accountGrade"
-          label="客户等级"
-          show-overflow-tooltip>
-        </el-table-column>
-        <el-table-column
-          prop="SalesRepresentative"
-          label="销售代表"
-          show-overflow-tooltip>
-        </el-table-column>
-        <el-table-column
-          prop="OrderTotal"
-          label="订单总额"
-          show-overflow-tooltip>
-        </el-table-column>
-        <el-table-column
-          prop="operating"
-          label="操作"
-          show-overflow-tooltip>
-        </el-table-column>
-      </el-table>
       <div style="margin-top: 20px">
         <el-pagination
           background
@@ -194,13 +186,72 @@ export default {
   },
   data () {
     return {
-      tableData3: [{
+      activeTabName: 'all',
+      accountTabs: [
+        {
+          label: '全部',
+          name: 'all'
+        },
+        {
+          label: '我负责',
+          name: 'responsible'
+        },
+        {
+          label: '我创建',
+          name: 'create'
+        },
+        {
+          label: '部门',
+          name: 'department'
+        }
+      ],
+      agentReportTable: [
+        {
+          name: 'Tom',
+          age: 12
+        },
+        {
+          name: 'James',
+          age: 13
+        }
+      ],
+      tableAccount: [{
         accountName: '张三的公司',
         accountSource: '渠道-自拓渠道',
         contactPerson: '李四',
         contactNumber: '13131324033',
         accountStatus: '服务中',
         accountGrade: '普通',
+        SalesRepresentative: '宇文化',
+        OrderTotal: '2000.00',
+        operating: '查看'
+      }, {
+        accountName: '李四的公司',
+        accountSource: '渠道-自拓渠道',
+        contactPerson: '李四',
+        contactNumber: '13131324033',
+        accountStatus: '服务中',
+        accountGrade: 'VIP',
+        SalesRepresentative: '宇文化',
+        OrderTotal: '2000.00',
+        operating: '查看'
+      }, {
+        accountName: '李四的公司',
+        accountSource: '渠道-自拓渠道',
+        contactPerson: '李四',
+        contactNumber: '13131324033',
+        accountStatus: '服务中',
+        accountGrade: 'VIP',
+        SalesRepresentative: '宇文化',
+        OrderTotal: '2000.00',
+        operating: '查看'
+      }, {
+        accountName: '李四的公司',
+        accountSource: '渠道-自拓渠道',
+        contactPerson: '李四',
+        contactNumber: '13131324033',
+        accountStatus: '服务中',
+        accountGrade: 'VIP',
         SalesRepresentative: '宇文化',
         OrderTotal: '2000.00',
         operating: '查看'
@@ -238,6 +289,17 @@ export default {
     },
     handleAdvancedSearch () {
       this.advancedSearchDialogVisible = true
+    },
+  },
+  computed: {
+    accountTabsData () {
+      if ( this.activeTabName === 'all') {
+        return this.tableAccount
+      } else if ( this.activeTabName === 'responsible') {
+        return this.tableAccount.filter(account => {
+          return account.contactPerson === '李四'
+        })
+      } else if ( this.acc)
     }
   }
 }
@@ -251,14 +313,6 @@ export default {
 }
 .account-header-custom{
   width: 100%;
-}
-.el-dropdown{
-  border: 1px solid #DCDFE6;
-  padding: 12px 24px;
-  border-radius:5px;
-}
-.el-button--primary{
-    padding: 12px 34px;
 }
 .el-row {
     margin-bottom: 20px;
@@ -295,18 +349,7 @@ export default {
   position: relative;
   top: 2px;
 }
-#base > div.account-header-custom > div > div:nth-child(2) > div > button{
-  padding: 10px 21px;
-}
-.el-table td, .el-table th{
-  padding: 19px 0;
-}
 .el-col-3{
   width:7.5%;
-}
-.account-home-dropdown{
-  border: 1px solid #DCDFE6;
-  padding: 10px 18px !important;
-  border-radius: 5px;
 }
 </style>
